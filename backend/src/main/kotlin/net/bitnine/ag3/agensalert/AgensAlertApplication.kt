5155b70e936bb70760411fc392a45ab12e8ab4a1
maxmin93 <maxmin93@gmail.com>
2020-04-09 19:23:24 +0900

    back: add some comments

--- backend/src/main/kotlin/net/bitnine/ag3/agensalert/AgensAlertApplication.kt
+++ backend/src/main/kotlin/net/bitnine/ag3/agensalert/AgensAlertApplication.kt
@@ -14,3 +14,12 @@ fun main(args: Array<String>) {
 // https://www.baeldung.com/spring-boot-kotlin-coroutines
 // https://github.com/razvn/webflux-r2dbc-kotlin
 // https://r2dbc.io/
+
+// **NOTE : 웹챗인데 이것은 참고만 할 것
+// https://github.com/swsms/webchat
+//
+// - WebMVC 와 WebFlux 는 하나의 Application 에서 혼용이 안된다
+// - 로그인 등의 복잡한 로직은 WebMVC 방식이 편하지만, 당장은 데모 목적이라 추가 안하기로
+// - 새로 추가된 그래프만 WebSocket 으로 전달하는, 단순기능으로 간다
+// - 급한 것은 Angular 쪽에 WebSocket 클라이언트와 Time-scrolling Chart
+
